4ee365f1d5f249c8ed9909d0867830fefc11a6a0
yoezer <dawayoezer22@gmail.com>
2024-12-10 12:57:20 +0600

    HMS-03:Added admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
 export const routes: Routes = [
     { path: '', component: SignupComponent }, // Default route
     { path: 'signup', component: SignupComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    {path: 'dashboard', component:DashBoardComponent},
 
 ];
