cda76a7c3082170e8d5dc6c317cffa17ddd4cd82
TsheyRin <niduptsherin@gmal.com>
2024-12-13 19:44:33 +0600

    HMS-18: Added guard and interceptor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,8 @@ import { SignupComponent } from './signup/signup.component';
 import { DashBoardComponent } from './dash-board/dash-board.component';
 
 export const routes: Routes = [
-    { path: '', component: SignupComponent }, // Default route
+    { path: '', component: LoginComponent }, // Default route
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent },
-    {path: 'dashboard', component:DashBoardComponent},
-
-];
+    { path: 'dashboard', component: DashBoardComponent },
+  ];
--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -1,19 +1,17 @@
 
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SignupModel } from '../models/signup.model';
+import { ApiService } from '../api.service';
 
 @Injectable({
-  providedIn: 'root'  
+  providedIn: 'root'
 })
 export class SignupService {
-  private apiUrl = 'http://localhost:8081/api/v1/users';  // Your backend API endpoint
-
-  constructor(private http: HttpClient) {}
+  constructor(private apiService: ApiService) {}
 
   signup(signupData: SignupModel): Observable<any> {
-    return this.http.post(this.apiUrl, signupData);  // Sends POST request to the backend
+    return this.apiService.post('/auth/sign-up', signupData);
   }
 }
